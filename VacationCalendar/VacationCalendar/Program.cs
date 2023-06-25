11f39b2632b13110d8d9bbcde94031fea06bc2e3
piotryfon <pit.pit@wp.pl>
2023-06-25 19:40:25 +0200

    Add folder VacationCalendar

---
+++ VacationCalendar/VacationCalendar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VacationCalendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
