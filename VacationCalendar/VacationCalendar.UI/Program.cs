174cb134851fdcadd9389d12e56d5ec9d45fb6f9
piotryfon <pit.pit@wp.pl>
2023-06-30 18:02:37 +0200

    Add do loop and possibility of re-election

--- VacationCalendar/VacationCalendar.UI/Program.cs
+++ VacationCalendar/VacationCalendar.UI/Program.cs
@@ -9,49 +9,76 @@ namespace VacationCalendar.UI
         static void Main(string[] args)
         {
             VacationRequest vacationRequest = new VacationRequest();
-
-            Console.WriteLine("Kalendarz urlopowy");
-            Console.WriteLine("\nWybierz opcję w menu:");
-            var menu = new Menu(new string[] { "Wystaw wniosek urlopowy", "Opcja_2", "Opcja_3", "Opcja_4", "Opcja_5", "Opcja_6" });
+           
+            var menu = new Menu(new string[] { "Wystaw wniosek urlopowy", "Opcja 2", "Opcja 3", "Opcja 4", "Opcja 5", "Exit" });
             var menuPainter = new ConsoleMenuPainter(menu);
 
-            bool done = false;
+            bool esc = true;
 
             do
             {
-                // położenie menu w konsoli
-                menuPainter.Paint(2, 3);
+                Console.WriteLine("Kalendarz urlopowy");
+                Console.WriteLine("\nWybierz opcję w menu:");
 
-                var keyInfo = Console.ReadKey();
+                bool done = false;
 
-                switch (keyInfo.Key)
+                do
                 {
-                    case ConsoleKey.UpArrow: menu.MoveUp(); break;
-                    case ConsoleKey.DownArrow: menu.MoveDown(); break;
-                    case ConsoleKey.Enter: done = true; break;
+                    // położenie menu w konsoli
+                    menuPainter.Paint(2, 3);
+
+                    var keyInfo = Console.ReadKey();
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow: menu.MoveUp(); break;
+                        case ConsoleKey.DownArrow: menu.MoveDown(); break;
+                        case ConsoleKey.Enter: done = true; break;
+                    }
                 }
-            }
-            while (!done);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Wybrano: " + (menu.SelectedOption ?? "Nie wybrano opcji z menu..."));
-            Console.ForegroundColor = ConsoleColor.Gray;
+                while (!done);
 
-            if (menu.SelectedIndex == 0)
-            {
-                Console.WriteLine("\nPodaj datę od kiedy? (dd/MM/rrrr)");
-                string from = Console.ReadLine();
-                Console.WriteLine("Podaj datę do kiedy? (dd/MM/rrrr)");
-                string to = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Wybrano: " + (menu.SelectedOption ?? "Nie wybrano opcji z menu..."));
+                Console.ForegroundColor = ConsoleColor.Gray;
 
-                string message;
-                var vacationDays = vacationRequest.CountVacationDays(from, to, out message);
+                if (menu.SelectedIndex == 0)
+                {
+                    Console.WriteLine("\nPodaj datę od kiedy? (dd/MM/rrrr)");
+                    string from = Console.ReadLine();
+                    Console.WriteLine("Podaj datę do kiedy? (dd/MM/rrrr)");
+                    string to = Console.ReadLine();
 
-                Console.WriteLine($"{message} {vacationDays}");
-            }
-            if (menu.SelectedIndex == 1)
-            {
-                Console.WriteLine("Opcja 2");
-            }
+                    string message;
+                    var vacationDays = vacationRequest.CountVacationDays(from, to, out message);
+
+                    Console.WriteLine($"{message} {vacationDays}");
+                }
+                if (menu.SelectedIndex == 1)
+                {
+                    Console.WriteLine("Wykonuje się opcja 2...");
+                }
+                if (menu.SelectedIndex == 2)
+                {
+                    Console.WriteLine("Wykonuje się opcja 3...");
+                }
+                if (menu.SelectedIndex == 3)
+                {
+                    Console.WriteLine("Wykonuje się opcja 4...");
+                }
+                if (menu.SelectedIndex == 4)
+                {
+                    Console.WriteLine("Wykonuje się opcja 5...");
+                }
+                if (menu.SelectedIndex == 5)
+                {
+                    esc = !esc;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (esc);
         }
     }
 }
