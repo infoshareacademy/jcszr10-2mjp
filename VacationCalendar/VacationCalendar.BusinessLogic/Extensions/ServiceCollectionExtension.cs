c4801d5308a0c5193c85e4610ac4684604be0b5c
piotryfon <pit.pit@wp.pl>
2023-08-18 13:46:32 +0200

    Move dbContext configuration and seeding to BusinessLogic

---
+++ VacationCalendar/VacationCalendar.BusinessLogic/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationCalendar.BusinessLogic.Data;
+using VacationCalendar.BusinessLogic.Seeders;
+
+namespace VacationCalendar.BusinessLogic.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddBusinessLogic(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<VacationCalendarDbContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("VacationCalendar")));
+
+            services.AddScoped<DataSeeder>();
+        }
+    }
+}
--- VacationCalendar/VacationCalendar.MVC/Program.cs
+++ VacationCalendar/VacationCalendar.MVC/Program.cs
@@ -1,26 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using VacationCalendar.BusinessLogic.Data;
+using VacationCalendar.BusinessLogic.Extensions;
 using VacationCalendar.BusinessLogic.Models;
+using VacationCalendar.BusinessLogic.Seeders;
 using VacationCalendar.BusinessLogic.Services;
 
 namespace VacationCalendar.MVC
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<VacationCalendarDbContext>(option => 
-                option.UseSqlServer(builder.Configuration.GetConnectionString("VacationCalendar")));
+            // rejestrowanie zale¿noœci z modu³u z logik¹ biznesow¹
+            builder.Services.AddBusinessLogic(builder.Configuration);
 
             builder.Services.AddScoped<IEmployeeService, EmployeeService>();
             builder.Services.AddScoped<IAdministratorService,  AdministratorService>();
 
             var app = builder.Build();
+
+            var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+            await seeder.Seed();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -40,51 +46,6 @@ namespace VacationCalendar.MVC
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            using var scope = app.Services.CreateScope();
-            var dbContext = scope.ServiceProvider.GetService<VacationCalendarDbContext>();
-            var pandingMigrations = dbContext.Database.GetPendingMigrations();
-            if (pandingMigrations.Any())
-            {
-                dbContext.Database.Migrate();
-            }
-
-            var managers = dbContext.Managers.ToList();
-            if (!managers.Any())
-            {
-                var manager1 = new Manager() { FirstName = "Anna", LastName = "Kowalska" };
-                var manager2 = new Manager() { FirstName = "Jan", LastName = "Kowalski" };
-                dbContext.Managers.AddRange(manager1, manager2);
-                dbContext.SaveChanges();
-            }
-
-            var employees = dbContext.Employees.ToList();
-            if (!employees.Any())
-            {
-                var employee1 = new Employee() { FirstName = "Magdalena", LastName = "Staniszewska", ManagerId = 1};
-                var employee2 = new Employee() { FirstName = "Piotr", LastName = "Tryfon", ManagerId = 1};
-                var employee3 = new Employee() { FirstName = "Jakub", LastName = "Szot", ManagerId = 2 };
-                dbContext.Employees.AddRange(employee1, employee2, employee3);
-                dbContext.SaveChanges();
-            }
-
-            var requestStatuses = dbContext.RequestStatuses.ToList();
-            if (!requestStatuses.Any())
-            {
-                var status1 = new RequestStatus() { RequestStatusName = "InProgress" };
-                var status2 = new RequestStatus() { RequestStatusName = "Confirmed" };
-                var status3 = new RequestStatus() { RequestStatusName = "Rejected" };
-                dbContext.RequestStatuses.AddRange(status1, status2, status3);  
-                dbContext.SaveChanges();
-            }
-
-            var admins = dbContext.Administrators.ToList();
-            if (!admins.Any())
-            {
-                var admin = new Administrator() { Login = "admin", Password = "password" };
-                dbContext.Administrators.Add(admin);
-                dbContext.SaveChanges();
-            }
 
             app.Run();
         }
