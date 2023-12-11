6404ead984b776d05d567a29f18875c077e0e695
piotryfon <pit.pit@wp.pl>
2023-12-11 22:27:56 +0100

    Add language switcher

---
+++ VacationCalendar/VacationCalendar.MVC/Models/LanguageSwitcherModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VacationCalendar.MVC.Models
+{
+    public class LanguageSwitcherViewModel
+    {
+        public string CurrentCulture { get; set; }
+        public IEnumerable<string> SupportedCultures { get; set; }
+    }
+}
---
+++ VacationCalendar/VacationCalendar.MVC/Views/Shared/Components/LanguageSwitcher/LanguageSwitcherViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using VacationCalendar.MVC.Models;
+
+namespace VacationCalendar.MVC.Views.Shared.Components.LanguageSwitcher
+{
+    public class LanguageSwitcherViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var currentCulture = CultureInfo.CurrentCulture.Name;
+
+            var model = new LanguageSwitcherViewModel
+            {
+                CurrentCulture = currentCulture,
+                SupportedCultures = new[] { "en-US", "pl-PL" }
+            };
+
+            return View("Default", model);
+        }
+    }
+}
